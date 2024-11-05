f71299b7cff9e381598a0aaa53fa61c05c05323c
20221tiimi0149 <guilhermesoaresvixbr@gmail.com>
2024-11-05 11:00:18 -0300

    second commit

--- app/src/main/java/soares/guilherme/galerapublica/MainActivity.java
+++ app/src/main/java/soares/guilherme/galerapublica/MainActivity.java
@@ -1,22 +1,59 @@
 package soares.guilherme.galerapublica;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.List;
+import java.util.ArrayList;
 
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private void checkForPermissions(){
+        List<String> permissionsNotGranted = new ArrayList<>();
+
+        if(permissionsNotGranted.size() > 0){
+
+        }
+        else{
+            MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+            int navigationOpSelected = vm.getNavigationOpSelected();
+            bottomNavigationView.setSelectedItemId(navigationOpSelected);
+        }
+    }
+
+    void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragContainer, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        checkForPermissions(permissions);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Toolbar toolbar = findViewById(R.id.tbMain);
-        setSupportActionBar(toolbar);
-
+        BottomNavigationView bottomNavigationView;
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -25,5 +62,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+       final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+
+       bottomNavigationView = findViewById(R.id.btNav);
+       bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               vm.setNavigationOpSelected(item.getItemId());
+               if(item.getItemId() == R.id.gridViewOp) {
+                   GridViewFragment gridViewFragment = GridViewFragment.newInstance();
+                   setFragment(gridViewFragment);
+               }
+               if (item.getItemId() == R.id.listViewOp) {
+                       ListViewFragment listViewFragment = ListViewFragment.newInstance();
+                       setFragment(listViewFragment);
+               }
+               return true;
+           }
+       });
     }
 }
